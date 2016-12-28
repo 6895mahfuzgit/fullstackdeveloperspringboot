5f065686d0e6e849c159c5987406e094600e59ff
6895mahfuzgit <6895mahfuz@gmail.com>
2016-12-28 13:20:05 +0600

    initialized

---
+++ src/main/java/com/fullstackdeveloperspringboot/FullstackdeveloperspringbootApplication.java
@@ -0,0 +1,12 @@
+package com.fullstackdeveloperspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackdeveloperspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackdeveloperspringbootApplication.class, args);
+	}
+}
